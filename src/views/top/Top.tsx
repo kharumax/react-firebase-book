2d104659def2a4d9d3692f11952e768318abf380
kharumax <kharumax@gmail.com>
2021-01-15 20:34:54 +0900

    Frameを修正*一部だけスクロール出来るように設定

--- src/views/top/Top.tsx
+++ src/views/top/Top.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import styles from "../../styles/top/Top.module.css";
 
 const Top: React.FC = () => {
+
+    var list = [];
+    for(var i=0; i<10; i++) {
+        list.push(<li>`${i}番目`</li>)
+    }
+
     return (
         <div className={styles.TopContainer}>
             <div className={styles.TopLeftSpaceContainer}></div>
             <div className={styles.TopLeftContainer}></div>
-            <div className={styles.TopCenterContainer}></div>
+            <div className={styles.TopCenterContainer}>
+                <ul>
+                    {
+                        list
+                    }
+                </ul>
+            </div>
             <div className={styles.TopRightContainer}></div>
             <div className={styles.TopRightSpaceContainer}></div>
         </div>

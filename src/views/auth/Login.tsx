349adafb4510b7bc32d5f710c4a38a743a0fb082
kharumax <kharumax@gmail.com>
2021-01-15 19:19:18 +0900

    認証系ページのUI作成完了

--- src/views/auth/Login.tsx
+++ src/views/auth/Login.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import styles from "../../styles/auth/Login.module.css";
+import TwitterIcon from "../../images/twitter-logo-white.png";
+
 
 const Login: React.FC = () => {
+
+    document.title = "Twitterにログイン / Twitter";
+
     return (
-        <div>
-            This is Login Page
+        <div className={styles.LoginContainer}>
+            <div className={styles.LoginInnerContainer}>
+                <img src={TwitterIcon}/>
+                <h2>Twitterにログイン</h2>
+                <input type="email" required placeholder="メールアドレス" className={styles.LoginEmailInput}/>
+                <input type="password" required minLength={6} placeholder="パスワード" className={styles.LoginPasswordInput}/>
+                <button type="submit" className={styles.LoginConfirmButton}>
+                    ログイン
+                </button>
+                <a href="/signup" className={styles.ToSignUpLink}>
+                    アカウント作成
+                </a>
+            </div>
         </div>
     );
 };
--- src/views/auth/SignUp.tsx
+++ src/views/auth/SignUp.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import styles from "../../styles/auth/SignUp.module.css";
+import TwitterIcon from "../../images/twitter-logo-white.png";
 
 const SignUp:React.FC = () => {
+    document.title = "Twitterに登録 / Twitter";
+
     return (
-        <div>
-            This is signUp Page
+        <div className={styles.SignUpContainer}>
+            <div className={styles.SignUpInnerContainer}>
+                <img src={TwitterIcon}/>
+                <h2>Twitterに登録</h2>
+                <input type="text" required placeholder="ユーザー名" className={styles.SignUpUsernameInput}/>
+                <input type="email" required placeholder="メールアドレス" className={styles.SignUpEmailInput}/>
+                <input type="password" required minLength={6} placeholder="パスワード" className={styles.SignUpPasswordInput}/>
+                <button type="submit" className={styles.SignUpConfirmButton}>
+                    登録
+                </button>
+                <a href="/login" className={styles.ToLoginLink}>
+                    ログインへ
+                </a>
+            </div>
         </div>
     );
 };

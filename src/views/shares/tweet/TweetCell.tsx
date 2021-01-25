dcbcdc4f5bef97107934e0eb0b9fd2b18fb1c8c3
kharumax <kharumax@gmail.com>
2021-01-25 17:06:24 +0900

    TweetCellの作成中

--- src/views/shares/tweet/TweetCell.tsx
+++ src/views/shares/tweet/TweetCell.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from "../../../styles/shares/tweet/TweetCell.module.css";
+import ProfileIcon from "../../../images/ironman.jpg";
 
 interface PROPS {
     title: string;
@@ -8,8 +9,20 @@ interface PROPS {
 const TweetCell: React.FC<PROPS> = (props) => {
 
     return (
-        <div>
-            <h2>{props.title}</h2>
+        <div className={styles.TweetCellContainer}>
+            <img src={ProfileIcon} alt="ProfileIcon" className={styles.TweetCellProfileImage}/>
+            <div className={styles.TweetCellContentContainer}>
+                <div className={}>
+
+                </div>
+                <div>
+
+                </div>
+                <img src={ProfileIcon} alt="PostImage" className={}/>
+                <div>
+
+                </div>
+            </div>
         </div>
     );
 };
